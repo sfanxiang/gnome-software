63f7b682f92a8e82510d9799a43d2305bca6869f
Richard Hughes <richard@hughsie.com>
2016-04-25 11:36:45 +0100

    Add gs_app_set_local_file()

--- src/gs-plugin-loader.c
+++ src/gs-plugin-loader.c
@@ -3827,6 +3827,7 @@ gs_plugin_loader_filename_to_app_thread_cb (GTask *task,
 	const gchar *function_name = "gs_plugin_filename_to_app";
 	gboolean ret = TRUE;
 	GError *error = NULL;
+	GList *l;
 	GsPluginLoaderAsyncState *state = (GsPluginLoaderAsyncState *) task_data;
 	GsPlugin *plugin;
 	GsPluginFilenameToAppFunc plugin_func = NULL;
@@ -3909,6 +3910,9 @@ gs_plugin_loader_filename_to_app_thread_cb (GTask *task,
  * Once the list of updates is refined, some of the #GsApp's of kind
  * %AS_APP_KIND_GENERIC will have been promoted to a kind of %AS_APP_KIND_DESKTOP,
  * or if they are core applications.
+ *
+ * Files that are supported will have the GFile used to create them available
+ * from the gs_app_get_local_file() method.
  **/
 void
 gs_plugin_loader_filename_to_app_async (GsPluginLoader *plugin_loader,
